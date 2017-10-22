b5d96a9430ac44b3acd22206c40b337d92b853c8
Haley West <haleykat09@yahoo.com>
2017-10-22 17:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ var object = {prop: 1}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {["prop2"]: '2'})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.prop2 = 2
+
+  return object 
+}
