941f42301d36305bed7174aecc4528dd5235f45a
Haley West <haleykat09@yahoo.com>
2017-10-22 17:28:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var recipes = {flour: "1 cup"}
 var object = {prop: 1}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, object, {["prop2"] = '2'})
+  return Object.assign({}, object, {["prop2"] = '2'})
 }
