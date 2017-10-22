3c6636ac8dd3aca79217d627f36ed2c89a9101c0
Haley West <haleykat09@yahoo.com>
2017-10-22 17:33:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ function deleteFromObjectByKey(object, key) {
   return Object.assign({}, "prop2", {})
 
 }
+
+function destructivelyDeleteFromObjectByKey( object, key) {
+  delete object["prop2"]
+}
