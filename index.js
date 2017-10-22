ebc3b24c1fcf74a7acf4be35111eb10a5dc666aa
Haley West <haleykat09@yahoo.com>
2017-10-22 17:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  return Object.assign({}, prop2, {})
+  return Object.assign({}, "prop2", {})
 
 }
+
